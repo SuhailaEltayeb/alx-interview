41f7125472783a4259a922638b442fdebc4393d1
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-10-22 19:53:33 +0000

    solving log parsing problem

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Solving Log parsing Dialemma
+Log parsing
 """
 import sys
 
 
-Line_Num = 0
-total_fsize = 0
+line_count = 0
+total_size = 0
 status_codes = {
     '200': 0,
     '301': 0,
@@ -21,9 +21,9 @@ status_codes = {
 
 def print_status():
     """
-    Function to print status
+    Helper function to output status
     """
-    print(f"File size: {total_fsize}")
+    print(f"File size: {total_size}")
 
     for code, count in status_codes.items():
         if count > 0:
@@ -32,9 +32,9 @@ def print_status():
 
 try:
     for line in sys.stdin:
-        if Line_No == 10:
+        if line_count == 10:
             print_status()
-            Line_No = 0
+            line_count = 0
 
         line = line.rstrip()
         line_parts = line.split()
@@ -43,13 +43,13 @@ try:
             code = line_parts[-2]
             total_size += int(line_parts[-1])
 
-            # Number of lines per status code
+            # Collate the number of lines per status code
             if code in status_codes:
                 status_codes[code] += 1
 
             # Increment the log count
-            Line_No += 1
+            line_count += 1
 except Exception:
     pass
 finally:
-    print_status()
+    print_status(
