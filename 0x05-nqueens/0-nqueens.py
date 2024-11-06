64faf7716e911b3acf4cda28124a6817187b1925
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-11-06 14:36:11 +0000

    0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Trying to solve N queens Dialemma
+"""
+import sys
+
+
+if __name__ == '__main__':
+
+    if (len(sys.argv) != 2):
+        print('Usage: nqueens N')
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        sys.exit(1)
